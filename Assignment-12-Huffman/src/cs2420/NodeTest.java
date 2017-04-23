d0750303a600f7f54932c4d54d9f477e57b03338
Anastasia Gonzalez <Anastasia@10.0.1.58>
2017-04-22 19:07:52 -0600

    finished testing the leaf and compareTo

--- Assignment-12-Huffman/src/cs2420/NodeTest.java
+++ Assignment-12-Huffman/src/cs2420/NodeTest.java
@@ -55,4 +55,32 @@ public class NodeTest {
 		assertEquals(four, five.get_right_child());
 	}
 	
+	@Test 
+	public void testLeftMost(){
+		Node one = new Node("a", 1);
+		Node two = new Node("b", 2);
+		Node three = new Node("c", one, two);
+		Node four = new Node("d", 3);
+		Node five = new Node("e", three, four);
+		Node six = new Node("e", 6);
+		
+		one.set_parent(three);
+		two.set_parent(three);
+		three.set_parent(five);
+		four.set_parent(five);
+		
+		int number = five.compareTo(six);
+		assertTrue(number < 0);
+	}
+	
+	@Test 
+	public void testLeaf(){
+		Node one = new Node("a", 1);
+		Node two = new Node("b", 2);
+		Node three = new Node("c", one, two);
+		
+		assertTrue(one.leaf());
+		assertFalse(three.leaf());
+	}
+	
 }
