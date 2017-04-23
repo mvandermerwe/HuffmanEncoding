ae0cdeff3e60743b200fae5121faeedc0ebceb4e
Anastasia Gonzalez <Anastasia@10.0.1.58>
2017-04-22 18:38:01 -0600

    test the right

--- Assignment-12-Huffman/src/cs2420/NodeTest.java
+++ Assignment-12-Huffman/src/cs2420/NodeTest.java
@@ -40,6 +40,19 @@ public class NodeTest {
 		
 		assertEquals(three, three.parents_left());
 		assertEquals(null, five.parents_left());
+		assertEquals(three, five.get_left_child());
+	}
+	
+	@Test 
+	public void testRightChild(){
+		Node one = new Node("a", 1);
+		Node two = new Node("b", 2);
+		Node three = new Node("c", one, two);
+		Node four = new Node("d", 3);
+		Node five = new Node("e", three, four);
+		three.set_parent(five);
+		
+		assertEquals(four, five.get_right_child());
 	}
 	
 }
